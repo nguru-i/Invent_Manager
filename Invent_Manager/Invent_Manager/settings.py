d7d160ac24d080e3a7a8e2e9ef039fd0cf6d5428
Ian Nguru <nguruiandavis@gmail.com>
2020-05-27 10:49:08 +0100

    created users app

--- Invent_Manager/Invent_Manager/settings.py
+++ Invent_Manager/Invent_Manager/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'stock.apps.StockConfig',
     'django.contrib.admin',
     'django.contrib.auth',
